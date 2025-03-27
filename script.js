730edddb5dd2e1275eb6ca54548818d560377dc9
OguzhanKadakal <76901135+OguzhanKadakal@users.noreply.github.com>
2025-03-27 18:17:35 +0300

    Enhance index.html structure with book addition button; update script.js with new book entries; modify style.css for layout adjustments and button styling

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const myLibrary = [
   new Book("The Hobbit", "J.R.R. Tolkien", "1937", "Fantasy"),
   new Book("1984", "George Orwell", "1949", "Dystopian"),
   new Book("To Kill a Mockingbird", "Harper Lee", "1960", "Fiction"),
+  new Book("Pride and Prejudice", "Jane Austen", "1813", "Romance"),
+  new Book("The Great Gatsby", "F. Scott Fitzgerald", "1925", "Tragedy"),
+  new Book("Moby-Dick", "Herman Melville", "1851", "Adventure"),
 ];
 
 function Book(title, author, year, genre, page) {
@@ -13,7 +16,4 @@ function Book(title, author, year, genre, page) {
   this.page = page;
 }
 
-
-function addBookToLibrary() {
-
-}
+function addBookToLibrary() {}
